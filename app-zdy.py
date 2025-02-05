ed9e57657a2b0a4fdb2e342b6d779e7f272e7f9d
zjq0501 <35991613+zjq0501@users.noreply.github.com>
2025-02-05 20:19:28 +0800

    Update app-zdy.py

--- app-zdy.py
+++ app-zdy.py
@@ -3,12 +3,11 @@ import pandas as pd
 import numpy as np  
 import joblib  
 import shap
-import lightgbm as lgb
 from streamlit_shap import st_shap
 
 # 初始化 session_state 中的 data
 if 'data' not in st.session_state:
-    st.session_state['data'] = pd.DataFrame(columns=['Gender','Age','CA15-3','CEA','CYFRA21-1','NSE','ALB','HDL','TG','Urea','Expectoration','Hemoptysis','Distress','Fever','Prediction', 'Label'])
+    st.session_state['data'] = pd.DataFrame(columns=['Gender', 'Age', 'CA15-3', 'CEA', 'CYFRA21-1', 'NSE', 'MCH', 'Expectoration', 'Hemoptysis', 'Fever', 'Duration','Prediction', 'Label'])
 
 # 设置页面为宽模式
 st.set_page_config(layout="wide")
@@ -46,12 +45,8 @@ with col1:
         a = st.selectbox("性别", options, index=0)
         a_val = gender_mapping['中文'][a]
         
-        b_col, label_col = st.columns([8,2])
-        b = b_col.number_input("年龄", min_value=0, max_value=100, value=62)
-        label_col.markdown("岁")
-        
         c_col, c_label_col = st.columns([8,2])
-        c = c_col.number_input("肿瘤相关抗原15-3", min_value=0.00, max_value=2000.00, value=12.50)
+        c = c_col.number_input("癌抗原15-3", min_value=0.00, max_value=2000.00, value=12.50)
         c_label_col.markdown("U/mL")
         
         d_col, d_label_col = st.columns([8,2])
@@ -67,16 +62,12 @@ with col1:
         f_label_col.markdown("ng/mL")
         
         g_col, g_label_col = st.columns([8,2])
-        g = g_col.number_input("白蛋白", min_value=0.0, max_value=100.0, value=39.7)
-        g_label_col.markdown("g/L")
+        g = g_col.number_input("平均红细胞血红蛋白含量", min_value=0.0, max_value=100.0, value=39.7)
+        g_label_col.markdown("pg")
     else:
         options = list(gender_mapping['English'].keys())
         a = st.selectbox("Gender", options, index=0)
         a_val = gender_mapping['English'][a]
-        
-        b_col, label_col = st.columns([8,2])
-        b = b_col.number_input("Age", min_value=0, max_value=100, value=62)
-        label_col.markdown("years")
         
         c_col, c_label_col = st.columns([8,2])
         c = c_col.number_input("CA15-3", min_value=0.00, max_value=2000.00, value=12.50)
@@ -95,65 +86,51 @@ with col1:
         f_label_col.markdown("ng/mL")
         
         g_col, g_label_col = st.columns([8,2])
-        g = g_col.number_input("ALB", min_value=0.0, max_value=100.0, value=39.7)
-        g_label_col.markdown("g/L")
+        g = g_col.number_input("MCH", min_value=0.0, max_value=100.0, value=39.7)
+        g_label_col.markdown("pg")
 
 with col2:
     if lang == '中文':
-        h_col, h_label_col = st.columns([8,2])
-        h = h_col.number_input("高密度脂蛋白", min_value=0.00, max_value=10.0, value=1.12)
-        h_label_col.markdown("mmol/L")
-        
-        i_col, i_label_col = st.columns([8,2])
-        i = i_col.number_input("甘油三酯", min_value=0.0, max_value=20.00, value=1.09)
-        i_label_col.markdown("mmol/L")
-        
-        j_col, j_label_col = st.columns([8,2])
-        j = j_col.number_input("血尿素", min_value=0.0, max_value=20.0, value=5.2)
-        j_label_col.markdown("mmol/L")
-        
         options = ["无", "有"]
+        b_col, label_col = st.columns([8,2])
+        b = b_col.number_input("年龄", min_value=0, max_value=100, value=62)
+        label_col.markdown("岁")		
+
         k = st.selectbox("咳痰", options, index=0)
         l = st.selectbox("咯血", options, index=0)
-        m = st.selectbox("胸闷", options, index=0)
-        n = st.selectbox("发热", options, index=0)
+        m = st.selectbox("发热", options, index=0)
+        n_col, n_label_col = st.columns([8,2])
+        n = n_col.number_input("吸烟年数", min_value=0, max_value=100, value=30)
+        n_label_col.markdown("年")
         
         k_val = 1 if k == "有" else 0
         l_val = 1 if l == "有" else 0
         m_val = 1 if m == "有" else 0
-        n_val = 1 if n == "有" else 0
     else:
-        h_col, h_label_col = st.columns([8,2])
-        h = h_col.number_input("HDL", min_value=0.00, max_value=10.0, value=1.12)
-        h_label_col.markdown("mmol/L")
-        
-        i_col, i_label_col = st.columns([8,2])
-        i = i_col.number_input("TG", min_value=0.0, max_value=20.00, value=1.09)
-        i_label_col.markdown("mmol/L")
-        
-        j_col, j_label_col = st.columns([8,2])
-        j = j_col.number_input("Urea", min_value=0.0, max_value=20.0, value=5.2)
-        j_label_col.markdown("mmol/L")
-        
         options = ["No", "Yes"]
+        b_col, label_col = st.columns([8,2])
+        b = b_col.number_input("Age", min_value=0, max_value=100, value=62)
+        label_col.markdown("years")
+
         k = st.selectbox("Expectoration", options, index=0)
         l = st.selectbox("Hemoptysis", options, index=0)
         m = st.selectbox("Distress", options, index=0)
-        n = st.selectbox("Fever", options, index=0)
+        n_col, n_label_col = st.columns([8,2])
+        n = n_col.number_input("Duration", min_value=0, max_value=100, value=30)
+        n_label_col.markdown("years")
         
         k_val = 1 if k == "Yes" else 0
         l_val = 1 if l == "Yes" else 0
         m_val = 1 if m == "Yes" else 0
-        n_val = 1 if n == "Yes" else 0
 
 # 定义不同语言下的因子名称
 feature_names = {
-    '中文': ['性别', '年龄', '肿瘤相关抗原15-3', '癌胚抗原', '细胞角蛋白19片段', '神经元特异性烯醇化酶', '白蛋白', '高密度脂蛋白', '甘油三酯', '血尿素', '咳痰', '咯血', '胸闷', '发热'],
-    'English': ['Gender', 'Age', 'CA15-3', 'CEA', 'CYFRA21-1', 'NSE', 'ALB', 'HDL', 'TG', 'Urea', 'Expectoration', 'Hemoptysis', 'Distress', 'Fever']
+    '中文': ['性别', '年龄', '癌抗原15-3', '癌胚抗原', '细胞角蛋白19片段', '神经元特异性烯醇化酶', '平均红细胞血红蛋白含量', '咳痰', '咯血', '发热', '吸烟年数'],
+    'English': ['Gender', 'Age', 'CA15-3', 'CEA', 'CYFRA21-1', 'NSE', 'MCH', 'Expectoration', 'Hemoptysis', 'Fever', 'Duration']
 }
 
 # Unpickle classifier    
-mm = joblib.load('LightGBM.pkl')
+mm = joblib.load('random_forest.pkl')
     
 # If button is pressed
 if lang == '中文':
@@ -163,8 +140,8 @@ else:
 
 if submit_button:
     # Store inputs into dataframe
-    X = pd.DataFrame([[a_val, b, c, d, e, f, g, h, i, j, k_val, l_val, m_val, n_val]], 
-                     columns=["Gender","Age","5A","8A","9A","10A","33A","42A","47A","50A","2BB","3BB","4BB","11BB"])
+    X = pd.DataFrame([[a_val, b, c, d, e, f, g, k_val,l_val,m_val,n]], 
+                     columns=["Gender", "Age", "CA15-3", "CEA", "CYFRA21-1", "NSE", "MCH", "Expectoration", "Hemoptysis", "Fever", "Duration"])
     
     # Get prediction
     result111 = mm.predict(X)
@@ -175,31 +152,18 @@ if submit_button:
     else:
         st.text(f"The probability of malignancy is: {round(result_prob_pos, 2)}%")
     
-    # SHAP分析
-    explainer = shap.TreeExplainer(mm) 
-    shap_values = explainer.shap_values(X)
-    
-    if isinstance(shap_values, list) and len(shap_values) == 2:
-        shap_values = shap_values[1]
-    else:
-        shap_values = shap_values
-    
-    shap_values = shap_values.reshape((1, -1))
-    
-    if isinstance(explainer.expected_value, list) and len(explainer.expected_value) == 2:
-        expected_value = explainer.expected_value[1]
-    else:
-        expected_value = explainer.expected_value
-    
-    # 绘制 SHAP force plot
+    # 生成SHAP值并绘图
     shap.initjs()
-    shap_plot = shap.force_plot(expected_value, shap_values, X.iloc[0], feature_names=feature_names[lang])
-    
-    # 显示图表
+    explainer = shap.TreeExplainer(mm) 
+    shap_values = explainer.shap_values(X,check_additivity=False)[0]
+    shap_values = shap_values[:,1] 
+    shap_values = shap_values.reshape((1, -1)) 
+    # 绘制 SHAP force plot
+    shap_plot = shap.force_plot(explainer.expected_value[1], shap_values[0], X.iloc[0], feature_names=feature_names[lang])
     st_shap(shap_plot, height=150, width=800)
-    
+	
     # 创建一个新的DataFrame来存储用户输入的数据
-    new_data = pd.DataFrame([[a, b, c, d, e, f, g, h, i, j, k, l, m, n, result_prob_pos/100, None]], 
+    new_data = pd.DataFrame([[a_val, b, c, d, e, f, g, k_val,l_val,m_val,n, result_prob_pos/100, None]], 
                             columns=st.session_state['data'].columns)
     
     # 将预测结果添加到新数据中
@@ -214,23 +178,23 @@ if uploaded_file is not None:
     column_mapping = {
         'Gender': 'Gender',
         'Age': 'Age',
-        'CA15-3': '5A',
-        'CEA': '8A',
-        'CYFRA21-1': '9A',
-        'NSE': '10A',
-        'ALB': '33A',
-        'HDL': '42A',
-        'TG': '47A',
-        'Urea': '50A',
-        'Expectoration': '2BB',
-        'Hemoptysis': '3BB',
-        'Distress': '4BB',
-        'Fever': '11BB'
+        'CA15-3': 'CA15-3',
+        'CEA': 'CEA',
+        'CYFRA21-1': 'CYFRA21-1',
+        'NSE': 'NSE',
+        'MCH': 'MCH',
+        'Expectoration': 'Expectoration',
+        'Hemoptysis': 'Hemoptysis',
+        'Fever': 'Fever',
+        'Duration': 'Duration'
     }
     
+	# 假设 'Label' 列在 Excel 文件中存在并且不参与计算
+    label_column = 'label'  # 这是 Excel 文件中未参与计算的列名
+	
     df = df.rename(columns=column_mapping)
     
-    required_cols = ["Gender","Age","5A","8A","9A","10A","33A","42A","47A","50A","2BB","3BB","4BB","11BB"]
+    required_cols = ["Gender","Age","CA15-3","CEA","CYFRA21-1","NSE","MCH","Expectoration","Hemoptysis","Fever","Duration"]
     missing_cols = [col for col in required_cols if col not in df.columns]
     
     if missing_cols:
@@ -241,7 +205,10 @@ if uploaded_file is not None:
             result = mm.predict(X)[0]
             result_prob = mm.predict_proba(X)[0][1]
             
-            new_data = pd.DataFrame([[row["Gender"], row["Age"], row["5A"], row["8A"], row["9A"], row["10A"], row["33A"], row["42A"], row["47A"], row["50A"], row["2BB"], row["3BB"], row["4BB"], row["11BB"], result_prob, None]], 
+			# 获取标签列的值
+            label = row[label_column] if label_column in row else None
+			
+            new_data = pd.DataFrame([[row["Gender"], row["Age"], row["CA15-3"], row["CEA"], row["CYFRA21-1"], row["NSE"], row["MCH"], row["Expectoration"], row["Hemoptysis"], row["Fever"], row["Duration"], result_prob, label]], 
                                     columns=st.session_state['data'].columns)
             st.session_state['data'] = pd.concat([st.session_state['data'], new_data], ignore_index=True)
     
